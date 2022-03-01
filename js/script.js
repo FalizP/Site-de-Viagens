029f1fa0b891f6e0fe836fb9bbe51bf7c81bbe68
Luiz Palma <luizpalma16@gmail.com>
2022-03-01 20:16:05 -0300

    seção marcas

--- js/script.js
+++ js/script.js
@@ -43,7 +43,7 @@ videoBtn.forEach((btn) => {
 });
 
 var swiper = new Swiper(".review-slider", {
-	spaceBetween: 23,
+	spaceBetween: 20,
 	loop: true,
 	autoplay: {
 		delay: 2500,
@@ -58,6 +58,29 @@ var swiper = new Swiper(".review-slider", {
 		},
 		1024: {
 			slidesPerView: 3,
+		},
+	},
+});
+
+var swiper = new Swiper(".brand-slider", {
+	spaceBetween: 20,
+	loop: true,
+	autoplay: {
+		delay: 2500,
+		disableOnInteraction: false,
+	},
+	breakpoints: {
+		450: {
+			slidesPerView: 2,
+		},
+		768: {
+			slidesPerView: 3,
+		},
+		991: {
+			slidesPerView: 4,
+		},
+		1200: {
+			slidesPerView: 5,
 		},
 	},
 });
